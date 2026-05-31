00b0764f0ddddbf71786e40d68a141313d580bbe
DanielGarcia6728 <viejochotano@gmail.com>
2026-05-31 17:36:53 -0500

    Add files via upload

---
+++ src/main/java/grupo/diseno/mindu/MinduApplication.java
@@ -0,0 +1,13 @@
+package grupo.diseno.mindu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinduApplication.class, args);
+	}
+
+}
